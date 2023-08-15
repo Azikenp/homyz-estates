fe8346c82b4907279bd8e19f579e94ca730e559f
Azikenp <azikenp26@gmail.com>
2023-08-15 20:47:26 +0100

    accordion finished and ciontact section start

--- src/components/value/Value.jsx
+++ src/components/value/Value.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
      Accordion, 
      AccordionItem, 
@@ -35,11 +35,30 @@ function Value() {
                     preExpanded={[0]}>
                         {
                             data.map((item, i) => {
+                                const [className, setClassName] = useState(null)
                                 return(
-                                    <AccordionItem className='accordion-item'>
+                                    <AccordionItem className={`accordionItem ${className}`} key={i} uuid={i}>
                                         <AccordionItemHeading>
-                                            
+                                            <AccordionItemButton className='flexCenter accordionButton'>
+
+                                                <AccordionItemState>
+                                                    {({expanded}) => 
+                                                        expanded
+                                                        ? setClassName("expanded") 
+                                                        : setClassName("collapsed")}
+                                                </AccordionItemState>
+                                                
+                                                <div className="flexCenter icon"> {item.icon} </div>
+                                                <span className="primaryText">{item.heading}</span>
+                                                <div className="flexCenter icon">
+                                                    <MdOutlineArrowDropDown size={20} />
+                                                </div>
+                                            </AccordionItemButton>
                                         </AccordionItemHeading>
+
+                                        <AccordionItemPanel>
+                                            <p className="secondaryText">{item.detail}</p>
+                                        </AccordionItemPanel>
                                     </AccordionItem>
                                 )
                             })
