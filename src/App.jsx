44345323bcba740dbd9771a2ed5533025772ddf7
Azikenp <azikenp26@gmail.com>
2023-08-15 15:10:22 +0100

    more changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import Header from "./components/header/Header"
 import Hero from "./components/hero/Hero";
+import Companies from "./components/companies/companies";
+import './App.css'
+import Residencies from "./components/residencies/Residencies";
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Hero />
+      <div>
+        <div className="white-gradient"></div>
+        <Header />
+        <Hero />
+      </div>
+      <Companies />
+      <Residencies/>
     </div>
   )
 }
