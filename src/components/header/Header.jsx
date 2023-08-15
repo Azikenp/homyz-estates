6f3b8371d9fdef46f013d0d91c34acfab15178dc
Azikenp <azikenp26@gmail.com>
2023-08-15 23:12:12 +0100

    responsiveness on contact page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,27 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {BiMenuAltRight} from 'react-icons/bi'
+import OutsideClickHandler from 'react-outside-click-handler';
 import './Header.css'
 
 
 function Header() {
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  const getMenuStyles = (menuOpened) => {
+    if(document.documentElement.clientWidth <= 800){
+      return {right: !menuOpened && "-100%"}
+    }
+  }
+  
   return (
     <section className="h-wrapper">
         <div className="h-container flexCenter paddings innerWidth">
-
-            <img src="./logo.png" alt="logo"  width ={100}/>
-            <div className="h-menu flexCenter">
-                <a href="">Residencies</a>
-                <a href="">Our Value</a>
-                <a href="">Contact Us</a>
-                <a href="">Get started</a>
-                <button className='button'>
-                    <a href="">Contact</a>
-                </button>
-            </div>
+          <img src="./logo.png" alt="logo"  width ={100}/>
+          <OutsideClickHandler onOutsideClick={() => setMenuOpened(false)}>
+            <div className="h-menu flexCenter"
+              style={getMenuStyles(menuOpened)}>
+                  <a href="">Residencies</a>
+                  <a href="">Our Value</a>
+                  <a href="">Contact Us</a>
+                  <a href="">Get started</a>
+                  <button className='button'>
+                      <a href="">Contact</a>
+                  </button>
+              </div>
+          </OutsideClickHandler>
+          
+          <div className="menu-icon" onClick={() => setMenuOpened((prev) => !prev)}>
+            <BiMenuAltRight size={30} />
+          </div>  
         </div>
-        <div className="menu-icon">
-          <BiMenuAltRight size={30} />
-        </div>
+        
     </section>
   )
 }
