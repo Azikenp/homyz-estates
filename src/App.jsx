6f6a5eb8480ee8261f216f2245445aa994472830
Azikenp <azikenp26@gmail.com>
2023-08-15 23:38:31 +0100

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/header/Header"
 import Hero from "./components/hero/Hero";
-import Companies from "./components/companies/companies";
+import Companies from "./components/companies/Companies";
 import './App.css'
 import Residencies from "./components/residencies/Residencies";
 import Value from "./components/value/Value";
