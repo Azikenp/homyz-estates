9b6e20ba5c9f82700a0e6c1aea96f15aa00e4a28
Azikenp <azikenp26@gmail.com>
2023-08-15 15:54:44 +0100

    accordion

--- src/components/value/Value.jsx
+++ src/components/value/Value.jsx
@@ -8,6 +8,7 @@ import {
      AccordionItemState 
     } from 'react-accessible-accordion';
 import 'react-accessible-accordion/dist/fancy-example.css';
+import data from '../../utils/accordion';
 import {MdOutlineArrowDropDown} from 'react-icons/md'
 import './Value.css'
 
@@ -27,6 +28,23 @@ function Value() {
                 <span className='orangeText'>Our Value</span>
                 <span className='primaryText'>Value We Give To You</span>
                 <span className='secondaryText'>We are always happy to help by providing the best services for you <br /> We believe a good place to live can make your life better </span>
+
+                <Accordion
+                    className='accordion'
+                    allowMultipleExpanded={false}
+                    preExpanded={[0]}>
+                        {
+                            data.map((item, i) => {
+                                return(
+                                    <AccordionItem className='accordion-item'>
+                                        <AccordionItemHeading>
+                                            
+                                        </AccordionItemHeading>
+                                    </AccordionItem>
+                                )
+                            })
+                        }
+                </Accordion>
             </div>
         </div>
     </div>
